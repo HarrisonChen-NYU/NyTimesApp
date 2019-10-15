2bee1d2836a9016fd3bf3f8abb8649089c0cfcdb
Harrison Chen <harrisonchen@Harrisons-MacBook-Air.local>
2019-10-16 00:37:56 +0800

    First Change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class= "nyTimes Page">
+
+    <div class= "one">
+    <div class= "date">
+      <div> Jan. 30, 2018</div>
+    </div>
+
+    <div class = "box">
+        <h3>The Shift</h3>
+        <h1>Kodak's Dubious Blockchain Gamble</h1>
+        <p1>What's a 130-year-old photo company doing dabbling 
+          in cryptopcurrency? Either revoluntionizing digital rights
+          management or trying to make a quick buck. </p1>
+        <h2>By KEVIN ROOSE</h2>
+    </div>
+
+    <div class= "photo">
+      <img class= "image" src = "https://on.mktw.net/32fwWtp" alt = "Kodak Blockchain"></img>
+    </div>
+    </div>
+
+    <div class= "two">
+    <div class= "date">
+      <div> Jan. 30, 2018</div>
+    </div>
+
+    <div class = "box">
+        <h1>Taiwan Retaliates Against Chinese Airlines, Hampering 
+          Lunar New Year Travel
+        </h1>
+        <p1>Taiwan, pushing back over encroachment on Taiwan Strait
+          airspace, may leave thousands without flights home 
+          for the holiday. </p1>
+        <h2>By CHRIS HORTON</h2>
+    </div>
+
+    <div class= "photo">
+      <img class= "image" src = "https://bit.ly/2pnjO6E" alt = "Taiwan"></img>
+    </div>
+    </div>
+
+    <div class = "three">
+    <div class= "date">
+      <div> Jan. 30, 2018</div>
+    </div>
+
+    <div class = "box">
+        <h1>New Jersey Embraces an Idea it Once Rejected: Make 
+          Utilities Pay to Emit Carbon
+        </h1>
+        <p1>Democratic governors nationwide are taking steps to 
+          tax or price emissions within their own borders, even
+          as Trump dismantles federal climate policy.</p1>
+        <h2>By BRAD PLUMER</h2>
+    </div>
+
+    <div class= "photo">
+      <img class= "image" src = "https://bit.ly/2VEhvbC" alt = "dTrump"></img>
+    </div>
+    </div>
     </div>
   );
 }
